baae52e1a4cff507139dd259d583567eedd1ba90
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2021-04-11 13:51:43 +0100

    [Rec-tester] Implemented HubTester and ping test

--- hub-tester/src/main/java/pt/tecnico/bicloin/hub/HubTester.java
+++ hub-tester/src/main/java/pt/tecnico/bicloin/hub/HubTester.java
@@ -1,5 +1,7 @@
 package pt.tecnico.bicloin.hub;
 
+import pt.tecnico.bicloin.hub.grpc.Hub.*;
+import io.grpc.StatusRuntimeException;
 
 public class HubTester {
 	
@@ -11,6 +13,29 @@ public class HubTester {
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
 		}
+
+		// check arguments
+		if (args.length < 2) {
+			System.out.println("Argument(s) missing!");
+			System.out.printf("Usage: java %s host port%n", HubTester.class.getName());
+			return;
+		}
+
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
+
+		HubFrontend frontend = new HubFrontend(host, port);
+		
+		try{
+			PingRequest request = PingRequest.newBuilder().setInput("Boss").build();
+			PingResponse response = frontend.ping(request);
+			System.out.println(response);
+		} catch (StatusRuntimeException e) {
+			System.out.println("Caught exception with description: " +
+			e.getStatus().getDescription());
+		}
+
+		frontend.close();
 	}
 	
 }
