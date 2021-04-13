e1e50dddbf58c765983860b07395128fa72279e0
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2021-04-13 22:41:44 +0100

    [Rec] Implement read method
    Added toString method to register
    
    Added debug

--- rec/src/main/java/pt/tecnico/rec/domain/Rec.java
+++ rec/src/main/java/pt/tecnico/rec/domain/Rec.java
@@ -4,11 +4,30 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import pt.tecnico.rec.grpc.Rec.*;
+import static pt.tecnico.rec.RecordMain.debug;
+
 
 public class Rec {
     private Map<String, Register> registers = new ConcurrentHashMap<>();
 
     public Rec() {
     }
+
+    private Register addNewRegister(String id) {
+        Register register = new Register();
+        registers.put(id, register);
+        return register;
+        
+    }
+
+    public synchronized void setRegister(String id, 
+            RegisterValue.ValueCase type, RegisterValue value) {
+
+        Register register = registers.containsKey(id) ? registers.get(id) : addNewRegister(id);
+
+        register.setValue(type, value);    
+        debug(register);    
+	}
+
 
 }
--- rec/src/main/java/pt/tecnico/rec/domain/Register.java
+++ rec/src/main/java/pt/tecnico/rec/domain/Register.java
@@ -3,12 +3,28 @@ package pt.tecnico.rec.domain;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static pt.tecnico.rec.RecordMain.debug;
+
 import pt.tecnico.rec.grpc.Rec.*;
 
 public class Register {
     Map<RegisterValue.ValueCase, RegisterValue> register = new ConcurrentHashMap<>();
 
     public Register() {
+    }
+
+    public void setValue(RegisterValue.ValueCase type, RegisterValue value) {
+        register.put(type, value);
+    }
+
+    @Override
+    public String toString() {
+        String output = "=== REGISTER CONTENT ===\n";
+        for (Map.Entry<RegisterValue.ValueCase, RegisterValue> entry : register.entrySet()) {
+            output += "Type: " + entry.getKey() +
+                                 "\nValue: " + entry.getValue();
+        }
+        return output;
     }
 
 }
