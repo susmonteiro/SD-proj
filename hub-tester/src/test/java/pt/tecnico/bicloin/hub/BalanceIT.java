583359f5388ee18dddf5358be1127c205718ce33
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2021-04-14 18:42:59 +0100

    [Hub-tester] Add balanceIT test

---
+++ hub-tester/src/test/java/pt/tecnico/bicloin/hub/BalanceIT.java
@@ -0,0 +1,39 @@
+package pt.tecnico.bicloin.hub;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static io.grpc.Status.UNAUTHENTICATED;
+import io.grpc.StatusRuntimeException;
+import pt.tecnico.bicloin.hub.grpc.Hub.*;
+
+public class BalanceIT extends BaseIT {
+    @Test
+    public void balanceSuccessTest() {
+        BalanceRequest request = BalanceRequest.newBuilder().setUserId("alice").build();
+        AmountResponse response = frontend.balance(request);
+        assertEquals(0, response.getBalance());
+    }
+
+    @Test
+    public void balanceNoSuchUserTest() {
+        BalanceRequest request = BalanceRequest.newBuilder().setUserId("f").build();
+        assertEquals(
+            UNAUTHENTICATED.getCode(),
+            assertThrows(StatusRuntimeException.class, () -> frontend.balance(request))
+            .getStatus().getCode()
+        );
+    }
+
+    @Test
+    public void balanceEmptyUserTest() {
+        BalanceRequest request = BalanceRequest.newBuilder().build();
+        assertEquals(
+            UNAUTHENTICATED.getCode(),
+            assertThrows(StatusRuntimeException.class, () -> frontend.balance(request))
+            .getStatus().getCode()
+        );
+    }
+}
