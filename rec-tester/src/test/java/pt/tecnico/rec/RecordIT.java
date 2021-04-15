038539b7f9db2f269197c8981c4c44c0e75d89fa
ist192456 <ist192456@tecnico.ulisboa.pt>
2021-04-15 23:34:40 +0100

    [Hub-tester] Implement tests, missing excpetions
    commit 17ff3cfdeb1ea8da952507c1b7aad2bc5dba9649
    Author: ist192456 <ist192456@tecnico.ulisboa.pt>
    Date:   Thu Apr 15 23:33:27 2021 +0100
    
        [Rec-tester] RecordIT, implment mixed tests
    
    commit 1891ad5fca03bb1462ff6e597466036c053cd070
    Author: ist192456 <ist192456@tecnico.ulisboa.pt>
    Date:   Thu Apr 15 23:33:03 2021 +0100
    
        [Rec-tester] ReadIT tmp disable unfinished tests
    
    commit ca12b58f55936a27090dfadb8fccb33e79bfdeb4
    Author: ist192456 <ist192456@tecnico.ulisboa.pt>
    Date:   Thu Apr 15 23:32:09 2021 +0100
    
        [Rec-tester] WriteIT implemented
    
    commit c08253b8e3f9acf17050c4b28de233d84be65d52
    Author: ist192456 <ist192456@tecnico.ulisboa.pt>
    Date:   Thu Apr 15 22:34:39 2021 +0100
    
        [Rec-tester] ReadIT change naming, unique ID
    
    commit d493d6c0f8bbed69f6bafad96f05a7098fdd6119
    Author: ist192456 <ist192456@tecnico.ulisboa.pt>
    Date:   Thu Apr 15 20:54:49 2021 +0100
    
        [Rec-tester] ReadIT change comment to generic
    
    commit 5ad460bd70e8e1dde08a3922d364644e48a5dbbd
    Author: ist192456 <ist192456@tecnico.ulisboa.pt>
    Date:   Thu Apr 15 19:23:08 2021 +0100
    
        [Hub-tester] Create Write test file
    
    commit 184ee3861858e55ae25d4f16ced962453440851f
    Author: ist192456 <ist192456@tecnico.ulisboa.pt>
    Date:   Thu Apr 15 19:18:48 2021 +0100
    
        [Rec-tester] Read test, TODO fill exception
    
    commit a73aa1b153c2e6c4ce3e9ace1d6a583b8a7764a4
    Author: ist192456 <ist192456@tecnico.ulisboa.pt>
    Date:   Thu Apr 15 18:55:37 2021 +0100
    
        [Rec-frontend] Made default values accessible in tests

--- rec-tester/src/test/java/pt/tecnico/rec/RecordIT.java
+++ rec-tester/src/test/java/pt/tecnico/rec/RecordIT.java
@@ -3,43 +3,50 @@ package pt.tecnico.rec;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class RecordIT {
-	
-	// static members
-	// TODO	
-	
-	
-	// one-time initialization and clean-up
-	@BeforeAll
-	public static void oneTimeSetUp(){
-		
-	}
-	
-	@AfterAll
-	public static void oneTimeTearDown() {
-		
-	}
-	
-	// initialization and clean-up for each test
-	
-	@BeforeEach
-	public void setUp() {
-		
-	}
-	
-	@AfterEach
-	public void tearDown() {
-		
-	}
-		
-	// tests 
+
+import static pt.tecnico.rec.frontend.RecordFrontend.*;
+
+public class RecordIT extends BaseIT {
 	
 	@Test
-	public void test() {
+	public void write_read_sameRegister_balance() {
+		int value = 1;
+		frontend.setBalance("RecordIT-write_read_sameRegister_balance", value);
+		int updatedVal = frontend.getBalance("RecordIT-write_read_sameRegister_balance");
+        
+		assertEquals(value, updatedVal);
+    }
+
+	@Test
+	public void read_write_read_sameRegister_onBike() {
+		boolean oldVal = frontend.getOnBike("RecordIT-read_write_read_sameRegister_onBike");
 		
+		boolean value = !oldVal;
+		frontend.setOnBike("RecordIT-read_write_read_sameRegister_onBike", value);
 		
-	}
+		boolean updatedVal = frontend.getOnBike("RecordIT-read_write_read_sameRegister_onBike");
+
+		assertNotEquals(oldVal, updatedVal);
+    }
+
+	@Test
+	public void repeatedRead_sameRegister_nBikes() {
+		int oldVal = frontend.getBalance("RecordIT-repeatedRead_sameRegister_nBikes");
+		int newVal = frontend.getBalance("RecordIT-repeatedRead_sameRegister_nBikes");
+        
+		assertEquals(oldVal, newVal);
+    }
+
+	@Test
+	public void repeatedRead_sameRegisterDifferentValue() {
+		int nPickUps = frontend.getNPickUps("RecordIT-repeatedRead_sameRegisterDifferentValue");
+		int nDeliveries = frontend.getNDeliveries("RecordIT-repeatedRead_sameRegisterDifferentValue");
+        
+		// Because a new register is created, both values should be default
+		assertEquals(getNPickUpsDefaultValue(), nPickUps);
+		assertEquals(getNDeliveriesDefaultValue(), nDeliveries);
+    }
 
 }
